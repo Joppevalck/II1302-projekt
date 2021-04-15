2c96a0ce0e9baf9144da048789b6cfe7593f43bc
Johannes Valck <johannesvalck@gmail.com>
2021-04-15 16:16:53 +0200

    Added vue app

---
+++ Website/smart-bin-site/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloBoys msg="Hello Pangpang Bois"/>
+  </div>
+</template>
+
+<script>
+import HelloBoys from './components/HelloBoys.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloBoys
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
