2672252e8a5e10d25faa9c49dd31f57467c36648
joakgus <joakgus@kth.se>
2021-05-11 16:54:58 +0200

    Added about us,logo, icon, router and removed documentation thing

--- webapp/src/views/Pages/AuthLayout.vue
+++ webapp/src/views/Pages/AuthLayout.vue
@@ -9,7 +9,7 @@
     >
       <div slot="brand" class="navbar-wrapper">
         <b-navbar-brand to="/">
-          <img src="img/brand/white.png">
+          <img src="img/brand/smartbin.png">
         </b-navbar-brand>
       </div>
 
@@ -34,18 +34,18 @@
                <i class="ni ni-planet"></i>
                <span class="nav-link-inner--text">Dashboard</span>
            </b-nav-item>
-           <b-nav-item to="/register">
+           <b-nav-item to="/aboutus">
                <i class="ni ni-circle-08"></i>
-               <span class="nav-link-inner--text">Register</span>
+               <span class="nav-link-inner--text">About Us</span>
            </b-nav-item>
-           <b-nav-item to="/login">
+           <!--<b-nav-item to="/login">
                <i class="ni ni-key-25"></i>
                <span class="nav-link-inner--text">Login</span>
            </b-nav-item>
            <b-nav-item to="/profile">
                <i class="ni ni-single-02"></i>
                <span class="nav-link-inner--text">Profile</span>
-           </b-nav-item>
+           </b-nav-item>-->
        </b-navbar-nav>
      </template>
     </base-nav>
@@ -64,23 +64,23 @@
         <b-row align-v="center" class="justify-content-xl-between">
           <b-col xl="6">
             <div class="copyright text-center text-xl-left text-muted">
-              © {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+              © {{year}} <a class="font-weight-bold ml-1" target="_blank">PangPangBois</a>
             </div>
           </b-col>
           <b-col xl="6" class="col-xl-6">
             <b-nav  class="nav-footer justify-content-center justify-content-xl-end">
-              <b-nav-item href="https://www.creative-tim.com" target="_blank" >
-                Creative Tim
+              <b-nav-item target="_blank" >
+                PangPangBois
               </b-nav-item>
-              <b-nav-item href="https://www.creative-tim.com/presentation" target="_blank" >
+              <b-nav-item  target="_blank" >
                 About Us
               </b-nav-item>
-              <b-nav-item href="http://blog.creative-tim.com"  target="_blank">
+              <!--<b-nav-item href="http://blog.creative-tim.com"  target="_blank">
                 Blog
               </b-nav-item>
               <b-nav-item href="https://www.creative-tim.com/license" target="_blank">
                 License
-              </b-nav-item>
+              </b-nav-item>-->
             </b-nav>
           </b-col>
         </b-row>
