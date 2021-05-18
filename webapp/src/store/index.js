22e11f6c4bb70d6fc881ec2f8b6388655240b82c
William Asp <wille.asp@live.se>
2021-05-18 16:53:49 +0200

    removed counter from store

--- webapp/src/store/index.js
+++ webapp/src/store/index.js
@@ -5,29 +5,19 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0,
     searchQuery: ""
   },
   mutations: {
-    increment (state) {
-      state.count++;
-    },
     setSearchQuery (state, s) {
         state.searchQuery = s;
     }
   },
   actions: {
-    increment (context) {
-      context.commit('increment')
-    },
     setSearchQuery (state, s) {
         state.commit('setSearchQuery', s);
     }
   },
   getters: {
-      getCount(state) {
-          return state.count;
-      },
       getSearchQuery (state) {
           return state.searchQuery;
       }
