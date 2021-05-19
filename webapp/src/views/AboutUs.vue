4601512fc5e76a8ce04219e4ea59f1b7b7f76222
Johannes Valck <johannesvalck@gmail.com>
2021-05-19 16:31:00 +0200

    done with about us

--- webapp/src/views/AboutUs.vue
+++ webapp/src/views/AboutUs.vue
@@ -5,21 +5,22 @@
       <b-container>
         <div class="header-body text-center mb-7">
           <b-row class="justify-content-center">
-            <b-col xl="5" lg="6" md="6" class="px-5">
+            <b-col xl="10" lg="6" md="6" class="px-5">
               <h1 class="text-light">We are the PangPangBois</h1>
               <p class="text-lead text-white">We are a group of students that has been given the opertunity to work together on a project. Thus project became this application. </p>
-              <h2 class="text-light">The team</h2>
-              
-              <div v-for="member in this.team" :key="member.name">
+              <h1 class="text-light">The Team</h1>
+              <b-row class="justify-content-center">
+              <b-col xl="4" lg="6" md="6" class="px-5" v-for="member in this.team" :key="member.name">
                 <h3 class="text-lead text-white">{{member.name}}</h3>
                 <h4 class="text-lead text-white">{{member.role}}, {{member.programme}}</h4>
-                <p>{{member.description}}</p>
-              </div>
+                <!-- <p>{{member.description}}</p> -->
+              </b-col>
+              </b-row>
               
-              <router-link to="/dashboard" class="font-weight-bold text-white mt-5">Back to dashboard</router-link>
             </b-col>
           </b-row>
         </div>
+
       </b-container>
       <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
