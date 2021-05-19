e022d695e17f1b57db5d4e01660b75bf220bb8aa
Johannes Valck <johannesvalck@gmail.com>
2021-05-19 14:38:55 +0200

    fixed footer and removed searchbar

--- webapp/src/views/DeviceSetup.vue
+++ webapp/src/views/DeviceSetup.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Header -->
-    <div class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-orange">
+    <div class="header bg-gradient-orange py-7 py-lg-8 pt-lg-9">
       <b-container>
         <div class="header-body text-center mb-7">
           <b-row class="justify-content-center">
@@ -11,35 +11,35 @@
             </b-col>
           </b-row>
         </div>
+      
+        <div align="center">
+          <b-form @submit="onSubmit" @reset="onReset">
+            <div class="col-md-6">
+                <b-form-input v-model="form.deviceId" placeholder="Device Id" required></b-form-input>
+              <p></p>
+            </div>
+            <div class="col-md-6">
+              <b-form-input v-model="form.name" placeholder="Name" required></b-form-input>
+              <p></p>
+            </div>
+              <div class="col-md-6">
+                  <b-form-input v-model="form.location" placeholder="Location" required></b-form-input>
+                <p></p>
+              </div>
+              <div class="col-md-6">
+                <b-form-input v-model="form.maxDist" placeholder="Maximum Distance" required></b-form-input>
+                <p></p>
+              </div>
+              <div class="col-md-6">
+                  <b-form-input v-model="form.minDist" placeholder="Minimum Distance" required></b-form-input>
+                <p></p>
+              </div>
+
+            <b-button type="submit">Submit</b-button>
+            <b-button type="reset">Reset</b-button>
+          </b-form>
+        </div>
       </b-container>
-
-
-      <div align="center">
-        <b-form @submit="onSubmit" @reset="onReset">
-          <div class="col-md-6">
-              <b-form-input v-model="form.deviceId" placeholder="Device Id" required></b-form-input>
-            <p></p>
-          </div>
-          <div class="col-md-6">
-            <b-form-input v-model="form.name" placeholder="Name" required></b-form-input>
-            <p></p>
-          </div>
-            <div class="col-md-6">
-                <b-form-input v-model="form.location" placeholder="Location" required></b-form-input>
-              <p></p>
-            </div>
-            <div class="col-md-6">
-              <b-form-input v-model="form.maxDist" placeholder="Maximum Distance" required></b-form-input>
-              <p></p>
-            </div>
-            <div class="col-md-6">
-                <b-form-input v-model="form.minDist" placeholder="Minimum Distance" required></b-form-input>
-              <p></p>
-            </div>
-          <b-button type="submit">Submit</b-button>
-          <b-button type="reset">Reset</b-button>
-        </b-form>
-      </div>
       <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
